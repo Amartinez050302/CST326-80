ddb95189c077afd7c8177183a171b139eebb2d1d
Adrian Martinez <adrianmartinez1@csumb.edu>
2023-03-20 18:58:24 -0700

    Nice

---
+++ TowerDefenseTutorial/Assets/LivesUi.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LivesUi : MonoBehaviour
+{
+    public TextMeshProUGUI livesText;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesText.SetText(PlayerStats.Lives.ToString() + " LIVES");
+    }
+}
